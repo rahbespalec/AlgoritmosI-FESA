5eddd61c01ca275912bda79a0764b2b30cc70fce
Raíssa Bespalec Daloia <79861204+rahbespalec@users.noreply.github.com>
2024-03-13 20:52:18 -0300

    Add files via upload

---
+++ AULA 1303 - exercício if/AULA 1303 - exercício if/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AULA_1303___exercício_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número entre 1-50");
+            Int32 numero = Int32.Parse(Console.ReadLine());
+
+           if (numero >= 0 && numero <=15 )
+            {
+                Console.WriteLine("Você está na faixa 1.");
+            }
+            else if (numero >= 0 && numero <= 25){
+                Console.WriteLine("Você está na faixa 2.");
+            }
+            else if (numero >= 0 && numero <= 50)
+            {
+                Console.WriteLine("Você está na faixa 3.");
+            }
+            else
+            {
+                Console.WriteLine("Você não está em nenhuma faixa");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula 13-03 - Idades/Aula 13-03 - Idades/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_13_03___Idades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é o seu nome?");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantos anos você tem?");
+            Int32 idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade == 0)
+            {
+                Console.Write($"Olá, {nome}! Você tem {idade} anos. Você é um bebê!");
+            }
+            else if (idade <= 12)
+            {
+                Console.Write($"Olá, {nome}! Você tem {idade} anos. Você é uma criança!");
+            }
+            else if (idade <= 17)
+            {
+                Console.Write($"Olá, {nome}! Você tem {idade} anos. Você é um adolescente!");
+            }
+            else if (idade <= 60)
+            {
+                Console.Write($"Olá, {nome}! Você tem {idade} anos. Você é um adulto!");
+            }
+            else if (idade <= 120)
+            {
+                Console.Write($"Olá, {nome}! Você tem {idade} anos. Você é um idoso!");
+            }
+            else
+            {
+                Console.Write($"Olá, {nome}! Você tem {idade} anos. Você é uma múmia!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
