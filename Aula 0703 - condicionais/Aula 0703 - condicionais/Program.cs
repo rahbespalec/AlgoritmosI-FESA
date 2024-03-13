bb359514d36770d23ae3a8069c6a5d372c6a9643
Raíssa Bespalec Daloia <79861204+rahbespalec@users.noreply.github.com>
2024-03-13 19:42:28 -0300

    Add files via upload

---
+++ Aula 0703 - condicionais/Aula 0703 - condicionais/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_0703___condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Int32 qtdEstoque, diferenca;
+
+            Console.Write("Quantos ovos existem no estoque?");
+            qtdEstoque = Convert.ToInt32(Console.ReadLine());
+
+            if(qtdEstoque > 12 && qtdEstoque < 36)
+                {
+                Console.WriteLine("Vou comprar mais ovos...");
+                qtdEstoque = qtdEstoque + 24;
+
+                }
+            else
+            {
+                if(qtdEstoque <12 )
+                {
+                    Console.WriteLine("Vou comprar mais que 24 ovos...");
+                    diferenca = 36 - qtdEstoque;
+                    qtdEstoque = qtdEstoque + diferenca;
+                }
+            }
+
+            Console.WriteLine($"Seu estoque está com {qtdEstoque} ovos");
+
+            Console.ReadKey();
+        }
+    }
+}
