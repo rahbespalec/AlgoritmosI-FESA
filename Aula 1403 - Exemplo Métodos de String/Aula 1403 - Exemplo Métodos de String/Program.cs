7f11e6bf1a633fc41562ab586d603f3dd1e485b5
Raíssa Bespalec Daloia <79861204+rahbespalec@users.noreply.github.com>
2024-03-14 22:43:13 -0300

    Add files via upload

---
+++ Aula 1403 - Exemplo Métodos de String/Aula 1403 - Exemplo Métodos de String/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1403___Exemplo_Métodos_de_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite uma frase: ");
+            String frase = Console.ReadLine();
+
+            Console.WriteLine($"Tudo minúsculo: {frase.ToLower()}");
+            Console.WriteLine($"Tudo maísculo: {frase.ToUpper()}");
+
+            if(frase.Length < 10)
+            {
+                Console.WriteLine("Esta frase possui menos que 10 letras");
+            }
+            else
+            {
+                Console.WriteLine("Esta frase é bem grande, hein?");
+            }
+            //Console.WriteLine($"As 4 primeiras letras: {frase.Substring(0, 4)}");
+
+            if (frase.IndexOf('u') == -1);
+            {
+                Console.WriteLine("A letra [u] não existe na frase");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula 1403 - Exercício de String/Aula 1403 - Exercício de String/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1403___Exercício_de_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o seu nome:");
+            String nome = Console.ReadLine();
+
+            string[] nameParts = nome.Split(' ');
+
+            // Assuming the first part is the first name
+            string PrimeiroNome = nameParts[0];
+            string Sobrenome = nameParts[nameParts.Length - 1];
+
+            Console.WriteLine("Olá, " + PrimeiroNome + "!");
+
+            if (nome.Length > 14)
+            {
+                Console.WriteLine("O seu nome possui mais do que 15 letras! Que grande, hein?");
+            }
+            else
+            {
+                Console.WriteLine("O seu nome é bem curtinho, né? Não tem nem 15 letras");
+            }
+            if (Sobrenome.Equals("Silva", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Olha, você tem o sobrenome mais famoso dessa nação!");
+            }
+            else
+            {
+                Console.WriteLine("Ahh, se fosse brasileiro tinha Siva no nome!");
+            }
+            Console.ReadKey();
+        }
+        
+
+            
+        }
+    }
+}
