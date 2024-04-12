5da1d624bf76f7bc343f1f2fa7a866422da1e5dd
Raíssa Bespalec Daloia <79861204+rahbespalec@users.noreply.github.com>
2024-04-11 22:42:36 -0300

    Add files via upload

---
+++ Aula 1104 - Validação de nota/Aula 1104 - Validação de nota/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1104___Validação_de_nota
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double nota1, nota2;
+
+            Console.WriteLine("Qual a sua nota da primeira prova? ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
