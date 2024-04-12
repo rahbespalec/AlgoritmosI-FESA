8da1196c3efa2236d9f15bfb496086c2c986a432
Raíssa Bespalec Daloia <79861204+rahbespalec@users.noreply.github.com>
2024-04-11 22:34:05 -0300

    Add files via upload

---
+++ Aula 1104 - Laço While/Aula 1104 - Laço While/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1104___Laço_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char tipoPagamento;
+
+            do
+            {
+                Console.WriteLine("Digite o tipo de pagamento ( a vista o parcelado) V/P: ");
+                tipoPagamento = Console.ReadLine().ToUpper()[0];
+
+                if (tipoPagamento != 'V' && tipoPagamento != 'P')
+                {
+                    Console.WriteLine("Valor errado, tente outro...");
+                }
+
+            }
+
+            while(tipoPagamento != 'V' && tipoPagamento != 'P');
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Aula 1104 - laços de repetição/Aula 1104 - laços de repetição/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1104___laços_de_repetição
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Int16 avaliacao;
+
+            Console.WriteLine("Informe uma nota para avaliação");
+            avaliacao = Int16.Parse(Console.ReadLine());
+
+            while (avaliacao < 0 || avaliacao > 5) 
+            {
+                Console.WriteLine("ERRO!!!!! Informe uma nota para avaliação entre 0 e 5:");
+                avaliacao= Int16.Parse(Console.ReadLine());
+            }
+
+            switch (avaliacao)
+            {
+                case 0:
+                    Console.WriteLine("Péssimo...");
+                    break;
+
+                case 1:
+                    Console.WriteLine("Ruim...");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Bom...");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Muito bom...");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Ótimo...");
+                    break;
+            }
+
+            Console.ReadKey();  
+
+
+        }
+    }
+}
