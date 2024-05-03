bbe83fc206e55dad561ce0ff7b242657013ca6e4
Raíssa Bespalec Daloia <79861204+rahbespalec@users.noreply.github.com>
2024-05-02 22:42:25 -0300

    Add files via upload

---
+++ Aula 0205 - ex 51/Aula 0205 - ex 51/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_0205___ex_51
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[5];
+            Double[] salarios = new Double[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Digite o nome da pessoa {i + 1}");
+                nomes[i] = Console.ReadLine();
+
+                Console.WriteLine($"Digite o salário da pessoa {i + 1}");
+                salarios[i] = Convert.ToDouble(Console.ReadLine());
+                
+            }
+
+            Console.WriteLine("\nNomes e salários:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{nomes[i]} - {salarios[i]}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
